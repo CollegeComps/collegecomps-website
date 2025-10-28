d6411f3267bd20d316f09939e1efe6e80259ab84
Franco Papalardo <francopapalardo93@gmail.com>
2025-10-28 00:33:45 -0500

    fix: add pkce and state checks to oauth providers (#125)

--- src/auth.ts
+++ src/auth.ts
@@ -70,6 +70,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   providers.push(Google({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    checks: ["pkce", "state"],
   }));
 }
 
@@ -77,6 +78,7 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
   providers.push(GitHub({
     clientId: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    checks: ["pkce", "state"],
   }));
 }
 
@@ -84,6 +86,7 @@ if (process.env.LINKEDIN_CLIENT_ID && process.env.LINKEDIN_CLIENT_SECRET) {
   providers.push(LinkedIn({
     clientId: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    checks: ["pkce", "state"],
   }));
 }
 
@@ -91,6 +94,7 @@ if (process.env.FACEBOOK_CLIENT_ID && process.env.FACEBOOK_CLIENT_SECRET) {
   providers.push(Facebook({
     clientId: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    checks: ["pkce", "state"],
   }));
 }
 
@@ -98,6 +102,7 @@ if (process.env.TWITTER_CLIENT_ID && process.env.TWITTER_CLIENT_SECRET) {
   providers.push(Twitter({
     clientId: process.env.TWITTER_CLIENT_ID,
     clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    checks: ["pkce", "state"],
   }));
 }
 
