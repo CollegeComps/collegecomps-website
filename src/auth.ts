2285b6b3cd7266de816c97abcf6dbdda8318c71a
Franco Papalardo <francopapalardo93@gmail.com>
2025-10-28 23:51:26 -0500

    fix: add usesecurecookies config for csrf handling

--- src/auth.ts
+++ src/auth.ts
@@ -130,6 +130,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   trustHost: true,
   providers,
+  useSecureCookies: process.env.NODE_ENV === 'production',
   callbacks: {
     async signIn({ user, account, profile }) {
       if (account && account.provider !== 'credentials') {
